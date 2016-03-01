c1da939bb09ebd90462db2a1cd921e3d5c4ef93e
rc420head <dennislebhardt@yahoo.com>
2016-02-29 23:00:27 -0600

    CandySiX rebase you are born

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -58,6 +58,6 @@ if [ "$TARGET_UNIFIED_DEVICE" == "" ] ; then
 fi
 echo "ro.build.characteristics=$TARGET_AAPT_CHARACTERISTICS"
 
-echo "ro.aospb.device=$AOSPB_DEVICE"
+echo "ro.candy.device=$CANDY_DEVICE"
 
 echo "# end build properties"
