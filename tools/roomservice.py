3fff2f21e3b028483b6f8a5f26e3e30d2221b02e
rc420head <dennislebhardt@yahoo.com>
2016-09-24 07:59:20 -0500

    Slim->Candy
    Change-Id: Ifc30b9ec972744bc1fca2633146825534f63762a

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -42,11 +42,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
+custom_local_manifest = ".repo/local_manifests/candy_manifest.xml"
 custom_default_revision = "ng7.0"
-custom_dependencies = "slim.dependencies"
-org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
-org_display = "SlimRoms"  # needed for displaying
+custom_dependencies = "candy.dependencies"
+org_manifest = "CandyRoms"  # leave empty if org is provided in manifest
+org_display = "CandyRoms"  # needed for displaying
 
 github_auth = None
 
